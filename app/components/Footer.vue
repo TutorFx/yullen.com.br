c3afba59ba07963a616bb78e95138325d85d0b8e
Gabriel Serejo <gabriel_serejo@live.com>
2024-10-13 18:33:33 -0400

    ✨ feat(footer): implements starter

---
+++ app/components/Footer.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { menuData } from '~/constants';
+import type { IMenu } from '~~/types';
+
+const data = ref<Record<string, IMenu[]>>({
+    menu: menuData,
+    blog: [
+
+    ]
+})
+</script>
+
+<template>
+    <div class="grid">
+        <div class="bg-white py-12">
+            <Container class="grid gap-6 grid-flow-col justify-center text-black items-center">
+                <div>
+                    <SmallLogo />
+                </div>
+                <div class="grid grid-flow-col gap-12">
+                    <div v-for="(item, i) in data" class="grid grid-flow-col justify-start gap-6">
+                        <div class="text-secondary font-bold py-1">
+                            {{ i.toUpperCase() }}
+                        </div>
+                        <div>
+                            <ul class="grid gap-3">
+                                <li v-for="(link, z) in item">
+                                    <Btn :to="link.to" color="inactive" size="sm">{{ link.text }}</Btn>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+        <div class="py-6 bg-black text-center text-balance">
+            Todos os direitos reservados pela licença Creative Commons 4.0 - Yullen Personal 2024
+        </div>
+    </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,5 +2,6 @@
   <div>
     <HeroVideo />
     <WhiteZone />
+    <Footer />
   </div>
 </template>
